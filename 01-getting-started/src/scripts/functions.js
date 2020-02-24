daf28fbabb5c564cdd0d9ccc34a1fe0bb47901d5
robsoza <robsoza1@gmail.com>
2020-02-24 13:55:44 -0700

    check-in comments

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -4,17 +4,26 @@ const functions = {
     size: (num) => {
         if (num < 10) return "small";
         if (num < 20) return "medium";
-        return "large";
+        if (num < 100) return "large";
+        return "extra large";
     },
 
     add: (num1, num2) => {
         return num1 + num2;
     },
 
+
+
     subtract: (num1, num2) => {
         return num1 - num2;
-    }
+    },
 
+    isEven: (num) => {
+        if (num % 2 == 0) {
+            return true;
+        };
+    return false;
+  },
     
 };
 
--- 01-getting-started/src/scripts/functions.test.js
+++ 01-getting-started/src/scripts/functions.test.js
@@ -6,10 +6,17 @@ test('Check the sizes', () => {
     expect(functions.size(10)).toBe("medium");
     expect(functions.size(15)).toBe("medium");
     expect(functions.size(20)).toBe("large");
-    expect(functions.size(2000000)).toBe("large");
+    expect(functions.size(80)).toBe("large");
+    expect(functions.size(2000000)).toBe("extra large");
+
 });
 
 test('Does that add function work?', () => {
     expect(functions.add(1,2)).toBe(3);
     expect(functions.add(101,202)).toBe(303);
+});
+
+test('Does it return true or false', () => {
+    expect(functions.isEven(1)).toBe(false);
+    expect(functions.isEven(2)).toBe(true);
 });
