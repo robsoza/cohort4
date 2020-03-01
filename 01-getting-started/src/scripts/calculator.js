bde30b73092ae7328e9dbfa0a12aa8c9b4bb498e
robsoza <robsoza1@gmail.com>
2020-03-01 15:57:31 -0700

    working on calculator

--- 01-getting-started/src/scripts/calculator.js
+++ 01-getting-started/src/scripts/calculator.js
@@ -1,87 +1,101 @@
-var n1 = document.getElementById("num1");
-var n2 = document.getElementById("num2");
-var button = document.getElementById("calculate");
-var addCheckBox = document.getElementById("+");
-var substractCheckBox = document.getElementById("-");
-var multiplyCheckBox = document.getElementById("*");
-var divideCheckBox = document.getElementById("/");
-var ul = document.getElementById("ul");
-var n1i = 0;
-var n2i = 0;
-
-function input1Length() {
-    return input1.value.length;
-}
-function input2Length() {
-    return input2.value.length;
-}
+let n1Input = document.getElementById("num1");
+let n2Input = document.getElementById("num2");
+let button = document.getElementById("calculate");
+let addCheckBox = document.getElementById("+");
+let substractCheckBox = document.getElementById("-");
+let multiplyCheckBox = document.getElementById("*");
+let divideCheckBox = document.getElementById("/");
+let ul = document.getElementById("ul-numbers");
+// let li = document.getElementById("logs");
+let num1 = 0;
+let num2 = 0;
 
 
-function addListAfterKeyPress(event) {
-    if (input1Length() > 0 && event.keyCode === 13) {
-        createListElement();
+
+// function input1Length() {
+//     return n1Input.value.length;
+// }
+// function input2Length() {
+//     return n2Input.value.length;
+// }
+
+// function addListAfterKeyPress(event) {
+//     if (input1Length() > 0 && event.keyCode === 13) {
+//         createListElement();
+//     }
+// }
+
+if (n1Input) {
+    n1Input.addEventListener("input", updateN1);
+    function updateN1(event) {
+        num1 = Number(event.target.value);
+        console.log(num1);
     }
 }
-
-
-
-n1.addEventListener("input", updateN1);
-function updateN1(event) {
-    n1i = Number(event.target.value);
-    //    console.log(n1i); 
-}
-
-n2.addEventListener("input", updateN2);
-function updateN2(event) {
-    n2i = Number(event.target.value);
-    // console.log(n2i);
-    document.getElementById("answer").value = (Number(n1.value) + Number(n2.value));
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(n1.value + " + " + n2.value + " = " + (Number(n1.value) + Number(n2.value))));
-    ul.appendChild(li);
-}
-
-
-addCheckBox.addEventListener("change", function () {
-    if (addCheckBox.checked) {
-        document.getElementById("answer").value = Number(n1.value) + Number(n2.value);
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(n1.value + " + " + n2.value + " = " + (Number(n1.value) + Number(n2.value))));
+if (n2Input) {
+    n2Input.addEventListener("input", updateN2);
+    function updateN2(event) {
+        num2 = Number(event.target.value);
+        document.getElementById("answer").value = num1 + num2;
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(num1 + " + " + num2 + " = " + (Number(num1) + Number(num2))));
         ul.appendChild(li);
     }
-})
 
-substractCheckBox.addEventListener("change", function () {
-    if (substractCheckBox.checked) {
-        document.getElementById("answer").value = Number(n1.value) - Number(n2.value);
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(n1.value + " - " + n2.value + " = " + (Number(n1.value) - Number(n2.value))));
-        ul.appendChild(li);
-    }
-})
+}
 
-multiplyCheckBox.addEventListener("change", function () {
-    if (multiplyCheckBox.checked) {
-        document.getElementById("answer").value = Number(n1.value) * Number(n2.value);
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(n1.value + " x " + n2.value + " = " + (Number(n1.value) * Number(n2.value))));
-        ul.appendChild(li);
-    }
-})
+if (addCheckBox) {
+    addCheckBox.addEventListener("change", function () {
+        if (addCheckBox.checked) {
+            document.getElementById("answer").value = num1 + num2;
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode(num1 + " + " + num2 + " = " + ((Number(num1) + Number(num2)))));
+            ul.appendChild(li);
+        }
+    })
+}
 
-divideCheckBox.addEventListener("change", function () {
-    if (divideCheckBox.checked) {
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(n1.value + " ÷ " + n2.value + " = " + (Number(n1.value) / Number(n2.value))));
-        ul.appendChild(li);
-    }
-})
+if (substractCheckBox) {
+    substractCheckBox.addEventListener("change", function () {
+        if (substractCheckBox.checked) {
+            document.getElementById("answer").value = num1 - num2;
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode(num1 + " - " + num2 + " = " + ((Number(num1) - Number(num2)))));
+            ul.appendChild(li);
+        }
+    })
+}
 
-button.addEventListener("click", function () {
-    n1.value = "";
-    n2.value = "";
-});
+if (multiplyCheckBox) {
+    multiplyCheckBox.addEventListener("change", function () {
+        if (multiplyCheckBox.checked) {
+            document.getElementById("answer").value = num1 * num2;
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode(num1 + " * " + num2 + " = " + ((Number(num1) * Number(num2)))));
+            ul.appendChild(li);
+        }
+    })
+}
 
+if (divideCheckBox) {
+    divideCheckBox.addEventListener("change", function () {
+        if (divideCheckBox.checked) {
+            document.getElementById("answer").value = num1 / num2;
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode(num1 + " / " + num2 + " = " + ((Number(num1) / Number(num2)))));
+            ul.appendChild(li);
+        }
+    })
+}
+
+if (button) {
+    button.addEventListener("click", function () {
+        window.location.reload('#calculate');
+        n1Input = "";
+        n2Input = "";
+
+    });
+}
 
 const calculatorFunctions = {
 
