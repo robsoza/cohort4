c9034f448b3cc032aed8b90d15a00d08978089e6
robsoza <robsoza1@gmail.com>
2020-03-02 11:32:40 -0700

    finished the calculator app

--- 01-getting-started/src/scripts/calculator.js
+++ 01-getting-started/src/scripts/calculator.js
@@ -1,51 +1,31 @@
 let n1Input = document.getElementById("num1");
 let n2Input = document.getElementById("num2");
 let button = document.getElementById("calculate");
-let addCheckBox = document.getElementById("+");
-let substractCheckBox = document.getElementById("-");
-let multiplyCheckBox = document.getElementById("*");
-let divideCheckBox = document.getElementById("/");
+let addCheckBox = document.getElementById("add-checkbox");
+let substractCheckBox = document.getElementById("substract-checkbox");
+let multiplyCheckBox = document.getElementById("multiply-checkbox");
+let divideCheckBox = document.getElementById("divide-checkbox");
 let ul = document.getElementById("ul-numbers");
-// let li = document.getElementById("logs");
+let li = document.getElementById("logs");
 let num1 = 0;
 let num2 = 0;
-
-
-
-// function input1Length() {
-//     return n1Input.value.length;
-// }
-// function input2Length() {
-//     return n2Input.value.length;
-// }
-
-// function addListAfterKeyPress(event) {
-//     if (input1Length() > 0 && event.keyCode === 13) {
-//         createListElement();
-//     }
-// }
 
 if (n1Input) {
     n1Input.addEventListener("input", updateN1);
     function updateN1(event) {
         num1 = Number(event.target.value);
-        console.log(num1);
     }
 }
+
 if (n2Input) {
     n2Input.addEventListener("input", updateN2);
     function updateN2(event) {
         num2 = Number(event.target.value);
-        document.getElementById("answer").value = num1 + num2;
-        let li = document.createElement("li");
-        li.appendChild(document.createTextNode(num1 + " + " + num2 + " = " + (Number(num1) + Number(num2))));
-        ul.appendChild(li);
     }
-
 }
 
 if (addCheckBox) {
-    addCheckBox.addEventListener("change", function () {
+    addCheckBox.addEventListener("click", function () {
         if (addCheckBox.checked) {
             document.getElementById("answer").value = num1 + num2;
             let li = document.createElement("li");
@@ -56,7 +36,7 @@ if (addCheckBox) {
 }
 
 if (substractCheckBox) {
-    substractCheckBox.addEventListener("change", function () {
+    substractCheckBox.addEventListener("click", function () {
         if (substractCheckBox.checked) {
             document.getElementById("answer").value = num1 - num2;
             let li = document.createElement("li");
@@ -67,7 +47,7 @@ if (substractCheckBox) {
 }
 
 if (multiplyCheckBox) {
-    multiplyCheckBox.addEventListener("change", function () {
+    multiplyCheckBox.addEventListener("click", function () {
         if (multiplyCheckBox.checked) {
             document.getElementById("answer").value = num1 * num2;
             let li = document.createElement("li");
@@ -78,7 +58,8 @@ if (multiplyCheckBox) {
 }
 
 if (divideCheckBox) {
-    divideCheckBox.addEventListener("change", function () {
+    divideCheckBox.addEventListener("click", function () {
+        console.log(divideCheckBox.checked);
         if (divideCheckBox.checked) {
             document.getElementById("answer").value = num1 / num2;
             let li = document.createElement("li");
@@ -87,13 +68,15 @@ if (divideCheckBox) {
         }
     })
 }
-
+console.log(li)
 if (button) {
     button.addEventListener("click", function () {
-        window.location.reload('#calculate');
-        n1Input = "";
-        n2Input = "";
-
+        window.location.href = '#calculator';
+        n1Input.value = "";
+        n2Input.value = "";
+        document.getElementById("answer").value = "";
+            ul.remove(li);
+        
     });
 }
 
@@ -113,11 +96,7 @@ const calculatorFunctions = {
 
     divide: (num1, num2) => {
         return num1 / num2;
-    },
-
-    lisenForNumber1: (num) => {
-        return listen1;
-    },
+    }
 };
 
 export default calculatorFunctions;
--- 01-getting-started/src/scripts/calculator.test.js
+++ 01-getting-started/src/scripts/calculator.test.js
@@ -1,16 +1,16 @@
 import calculatorFunctions from './calculator'
 
 test('Does that subtract function work?', () => {
-    expect(calculatorFunctions.add(1,2)).toBe(3);
-    expect(calculatorFunctions.add(101,202)).toBe(303);
+    expect(calculatorFunctions.add(1, 2)).toBe(3);
+    expect(calculatorFunctions.add(101, 202)).toBe(303);
 });
 
 test('Does that multiply function work?', () => {
-    expect(calculatorFunctions.multiply(3,2)).toBe(6);
-    expect(calculatorFunctions.multiply(10,5)).toBe(50);
+    expect(calculatorFunctions.multiply(3, 2)).toBe(6);
+    expect(calculatorFunctions.multiply(10, 5)).toBe(50);
 });
 
 test('Does that divide function work?', () => {
-    expect(calculatorFunctions.divide(6,2)).toBe(3);
-    expect(calculatorFunctions.divide(10,5)).toBe(2);
+    expect(calculatorFunctions.divide(6, 2)).toBe(3);
+    expect(calculatorFunctions.divide(10, 5)).toBe(2);
 });
