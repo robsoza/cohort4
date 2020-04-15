57fa6b02d71ca9ba8c8e4597140573befd3d48eb
robsoza <robsoza1@gmail.com>
2020-04-14 18:49:36 -0600

    130D - Exercise - Community and City TDD

--- 03-objects/src/scripts/city.js
+++ 03-objects/src/scripts/city.js
@@ -5,15 +5,37 @@
 
 class City {
 
-    constructor(name, lat, long, pop) {
+    constructor(name, lat, long, population) {
         this.name = name;
         this.latitude = lat;
         this.longitude = long;
-        this.population = pop;
+        this.population = population;
     }
 
     show() {
         return `City: ${this.name}, Lat: ${this.latitude}, Long: ${this.longitude}, Poulation: ${this.population}`
+    }
+
+    movedIn(num) {
+        this.population += Number(num);
+    }
+
+    movedOut(num) {
+        this.population -= Number(num);
+    }
+
+    howBig() {
+        if (this.population > 100000) {
+            return "City";
+        } if (this.population >= 20000 && this.population < 100000) {
+            return "Large Town";
+        } if (this.population >= 1000 && this.population < 20000) {
+            return "Town";
+        } if (this.population >= 100 && this.population < 1000) {
+            return "Village";
+        } else if (this.population >= 1 && this.population < 100) {
+            return "Hamlet";
+        }
     }
 }
 
@@ -21,6 +43,11 @@ class Community {
 
     constructor() {
         this.comms = [];
+    }
+
+    addCity(name, lat, long, population) {
+        let postCity = new City(name, lat, long, population);
+        fetch(ulr)
     }
 }
 
