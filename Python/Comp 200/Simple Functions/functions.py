f8cc2a622a12e9e42efef74d7571b88a7fec0462
robsoza <robsoza1@gmail.com>
2020-07-06 22:20:59 -0600

    Working on Comp 220 Python File IO

--- Python/Comp 200/Simple Functions/functions.py
+++ Python/Comp 200/Simple Functions/functions.py
@@ -1,13 +1,21 @@
+from decimal import *
+
 # email function
+
+
 def email(fname, lname):
-    return f"{fname}{lname}@evolveu.ca"
+    if not fname.strip() or not lname.strip():
+        return "Missing Arguments"
+    else:
+        return f"{fname}{lname}@evolveu.ca"
 
 
 # tax calculator
 data = {48535: 0.15, 48534: 0.205, 53404: 0.26,
         63895: 0.29, float('inf'): 0.33}
 
-def tax(income):
+
+def cantax(income):
     tax = 0
     if type(income) is str or income == None:
         return 0
@@ -18,4 +26,6 @@ def tax(income):
             if income > 0:
                 tax += (income * rate)
         income -= bracket
-    return round(tax, 2)
+    tax = Decimal(tax)
+    tax = tax.quantize(Decimal('0.000'), rounding=ROUND_UP)
+    return float(round(tax, 2))
--- Python/Comp 200/Simple Functions/functions_test.py
+++ Python/Comp 200/Simple Functions/functions_test.py
@@ -2,43 +2,46 @@ import pytest
 import functions
 
 # email function
+
+
 def test_email():
+    assert functions.email("", "") == "Missing Arguments"
     assert functions.email("rob", "soza") == "robsoza@evolveu.ca"
 
 
 # tax calculator
-def test_email():
+def test_cantax():
     # first bracket
-    assert functions.tax(1) == 0.15
-    assert functions.tax(50) == 7.5
-    assert functions.tax(20000) == 3000
-    assert functions.tax(48535) == 7280.25
-    
+    assert functions.cantax(1) == 0.15
+    assert functions.cantax(50) == 7.50
+    assert functions.cantax(20000) == 3000
+    assert functions.cantax(48535) == 7280.25
+
     # second bracket
-    assert functions.tax(48536) == 7280.45
-    assert functions.tax(50000) == 7580.57
-    assert functions.tax(80000) == 13730.58
-    assert functions.tax(97069) == 17229.72
-    
+    assert functions.cantax(48536) == 7280.46
+    assert functions.cantax(50000) == 7580.58
+    assert functions.cantax(80000) == 13730.58
+    assert functions.cantax(97069) == 17229.72
+
     # third bracket
-    assert functions.tax(97070) == 17229.98
-    assert functions.tax(100000) == 17991.78
-    assert functions.tax(120000) == 23191.78
-    assert functions.tax(150473) == 31114.76
-    
+    assert functions.cantax(97070) == 17229.98
+    assert functions.cantax(100000) == 17991.78
+    assert functions.cantax(120000) == 23191.78
+    assert functions.cantax(150473) == 31114.76
+
     # fourth bracket
-    assert functions.tax(150474) == 31115.05
-    assert functions.tax(175000) == 38227.59
-    assert functions.tax(200000) == 45477.59
-    assert functions.tax(214368) == 49644.31
-    
+    assert functions.cantax(150474) == 31115.05
+    assert functions.cantax(175000) == 38227.59
+    assert functions.cantax(200000) == 45477.59
+    assert functions.cantax(214368) == 49644.31
+
     # fith bracket
-    assert functions.tax(214369) == 49644.64
-    assert functions.tax(250000) == 61402.87
-    assert functions.tax(300000) == 77902.87
-    assert functions.tax(10000000) == 3278902.87
-    
+    assert functions.cantax(214369) == 49644.64
+    assert functions.cantax(250000) == 61402.87
+    assert functions.cantax(300000) == 77902.87
+    assert functions.cantax(10000000) == 3278902.87
+
     # random things
-    assert functions.tax(-1) == 0
-    assert functions.tax("apples") == 0
-    assert functions.tax(None) == 0
+    assert functions.cantax(-1) == 0
+    assert functions.cantax("apples") == 0
+    assert functions.cantax(None) == 0
